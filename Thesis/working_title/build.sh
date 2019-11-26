ee10c9a5a1fb35489d8174058225f08e7204c813
Dominik <meistalampe.dl@gmail.com>
2019-11-26 11:28:23 +0100

    thesis folder added

---
+++ Thesis/working_title/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# A LaTeX toolchain : PDFLaTeX -> BibTeX -> MakeIndex (Glossaries) -> MakeIndex (Acronyms) -> PDFLaTeX -> PDFLaTeX
+
+echo "Building ..."
+pdflatex -synctex=1 -interaction=batchmode $1.tex 
+bibtex $1
+makeindex -s $1.ist -t $1.glg -o $1.gls $1.glo
+makeindex -s $1.ist -t $1.alg -o $1.acr $1.acn
+pdflatex -synctex=1 -interaction=batchmode $1.tex
+pdflatex -synctex=1 -interaction=batchmode $1.tex
+echo "Cleaning up ..."
+rm $1.acn $1.acr $1.alg $1.aux $1.bbl $1.blg $1.glg $1.glo $1.gls $1.synctex.gz $1.ist $1.lof $1.log $1.lot $1.out $1.toc
+echo "Done."
